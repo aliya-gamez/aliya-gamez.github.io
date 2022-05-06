6622110f3a97061100b8718537cb2815b5981f92
Aliya Gamez <aliya.e.gamez@gmail.com>
2022-05-06 13:59:24 -0400

    edit scroll

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
 var element = document.querySelector("#post-container");
 
-// smooth scroll to element and align it at the bottom
-element.scrollIntoView({ behavior: 'smooth', block: 'end'});
+element.scrollIntoView();
